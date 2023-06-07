d838f9489395804ab9ed4101899e25cb57fa7e29
sven <andersensven.star@gmail.com>
2023-06-07 22:02:35 +0800

    Got buildings.json

--- src/pages/Buildings/Buildings.js
+++ src/pages/Buildings/Buildings.js
@@ -1,4 +1,23 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
 const Buildings = () => {
+
+    const[buildings, setBuildings] = useState([]);
+
+    useEffect(() => {
+        const fectchBuildings = async () => {
+            const res = await axios.get('./buildings.json');
+            setBuildings(res.data.buildings);
+        };
+        fectchBuildings();
+    }, []);
+
+    useEffect(() => {
+    console.log(buildings);
+        
+    }, [buildings])
+
     return (
         <h1>Buildings</h1>
     )
