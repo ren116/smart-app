824a6b4310a8b1c5e6fbed344e092d294b908ab8
sven <andersensven.star@gmail.com>
2023-06-07 19:52:26 +0800

    fixed page name as buildings

--- src/pages/Building/Building.js
+++
@@ -1,7 +0,0 @@
-const Building = () => {
-    return (
-        <h1>Buildings</h1>
-    )
-}
-
-export default Building;
---
+++ src/pages/Buildings/Buildings.js
@@ -0,0 +1,7 @@
+const Buildings = () => {
+    return (
+        <h1>Buildings</h1>
+    )
+}
+
+export default Buildings;
