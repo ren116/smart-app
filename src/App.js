f54f322880836d2aee5fd785f5a8c3f79eb1691e
aliceibrahimovic416 <aliceibrahimovic416@gmail.com>
2023-06-08 08:36:32 +0900

    Add make scroll-primary of Nfts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "pages/Layout";
 import Home from "pages/Home";
 import NoPage from "pages/NoPage";
+import Nfts from "pages/Nfts";
 import "./App.css";
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
+            {/* <Route path="*" element={<NoPage />} /> */}
+            <Route path="*" element={<Nfts />} />
             <Route index element={<Home />} />
-            <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/Nfts/index.js
+++ src/pages/Nfts/index.js
@@ -1,88 +1,75 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import Container from "@material-ui/core/Container";
+import Input from "@mui/material/Input";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 
 const Nfts = () => {
   const [nfts, setNfts] = useState([]);
-  const [page, setPage] = useState(0);
-  const [loading, setLoading] = useState(false);
+  const [filteredNfts, setFilteredNfts] = useState([]);
+  const [offset, setOffset] = useState(0);
   const [searchQuery, setSearchQuery] = useState("");
 
-  const observer = useRef();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-
-      const response = await fetch(
-        `https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=20&offset=${
-          page * 20
-        }`
-      );
-      const data = await response.json();
-      console.log(data);
-
-      setNfts((prevNfts) => [...prevNfts, ...data.results]);
-      setLoading(false);
-    };
-
-    fetchData();
-  }, [page]);
-
-  useEffect(() => {
-    observer.current = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting && !loading) {
-          setPage((prevPage) => prevPage + 1);
-        }
-      },
-      { threshold: 1 }
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=20&offset=${offset}`
     );
-  }, [loading]);
+    const data = await response.json();
+    console.log(data);
+    setNfts([...nfts, ...data.results]);
+    setFilteredNfts([...nfts, ...data.results]);
+    setOffset(offset + 20);
+  };
 
   useEffect(() => {
-    if (observer.current) {
-      observer.current.disconnect();
-    }
-    const target = document.querySelector("#end-of-list");
-    if (target) {
-      observer.current.observe(target);
-    }
+    fetchData();
   }, []);
 
-  const filteredNfts = nfts.filter((nft) =>
-    nft.collectionTitle.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+  useEffect(() => {
+    setFilteredNfts(
+      nfts.filter((nfts) =>
+        nfts.collectionTitle.toLowerCase().includes(searchQuery)
+      )
+    );
+  }, [nfts, searchQuery]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 1000
+      ) {
+        fetchData();
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [offset]);
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value.toLowerCase());
+  };
 
   return (
     <>
       <TextField
         label="Search NFTs"
         value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
+        m={{top:5,bottom:5}}
+        onChange={handleSearch}
       />
-
-      {/* <Grid container spacing={2}>
-        {filteredNfts.map((nft, index) => (
-          <Grid item key={nft.id} xs={2} sm={4} md={4} lg={4}>
-            <img src={nft.img} width="100%" height="100%" alt=""/>
-            <h3>{nft.collectionTitle}</h3>
-            <p>{nft.price}</p>
-
-            {index === filteredNfts.length - 1 && <div id="end-of-list" />}
-          </Grid>
-        ))}
-      </Grid> */}
       <Grid
         container
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 2, sm: 8, md: 16, xl: 20 }}
       >
-        {filteredNfts.map((nft, index) => (
-          <Grid item key={nft.id} xs={2} sm={4} md={4} lg={4} xl={5}>
-            <img src={nft.img} width="100%" height="80%" alt=""/>
-            <h4>{nft.collectionTitle}</h4>
-            <p>{nft.price}</p>
+        {filteredNfts.map((nfts, index) => (
+          <Grid item key={nfts.id} xs={2} sm={4} md={4} lg={4} xl={5}>
+            <img src={nfts.img} width="100%" height="80%" alt="" />
+            <h4>{nfts.collectionTitle}</h4>
+            <p>{nfts.price}</p>
 
             {index === filteredNfts.length - 1 && <div id="end-of-list" />}
           </Grid>
