21850abe3f4aeee1bfba3e1915b16e96475c0fe2
pabaross <pababravo@gmail.com>
2023-06-08 23:08:31 +0900

    update NFTs page to show NFTs List,  create NFT card Model which has name ,price and image.

--- src/pages/NFTs/NFTs.js
+++ src/pages/NFTs/NFTs.js
@@ -1,8 +1,55 @@
 export default function NFTs() {
     return (
-    <div className="nfts">
-        <h1>NFTs Market Place</h1>
+        <div className="nfts">
+            <div className="nfts_seacrh">
+                <h1>NFTs Market Place</h1>
+                <input placeholder="search nfts name" />
+            </div>
+            <div className="nfts_list" style={{ display: "flex", justifyContent: "center" }}>
+                <div className="nfts_data" style={{ width: "80%", display: "flex", flexWrap: "wrap" }} >
+                    <div className="card" style={{ width: "20%", height: "100px", padding: "10px" }}>
+                        <img src="image source" class="img-fluid rounded-top" alt="img" style={{ width: "100%", height: "100px" }} />
+                        <p>Name</p>
+                        <p>Price</p>
+                    </div>
 
-    </div>
+                    <div className="card" style={{ width: "20%", padding: "10px" }}>
+                        <img src="image source" class="img-fluid rounded-top" alt="img" style={{ width: "100%", height: "100px" }} />
+                        <p>Name</p>
+                        <p>Price</p>
+                    </div>
+                    <div className="card" style={{ width: "20%", padding: "10px" }}>
+                        <img src="image source" class="img-fluid rounded-top" alt="img" style={{ width: "100%", height: "100px" }} />
+                        <p>Name</p>
+                        <p>Price</p>
+                    </div>
+                    <div className="card" style={{ width: "20%", padding: "10px" }}>
+                        <img src="image source" class="img-fluid rounded-top" alt="img" style={{ width: "100%", height: "100px" }} />
+                        <p>Name</p>
+                        <p>Price</p>
+                    </div>
+                    <div className="card" style={{ width: "20%", padding: "10px" }}>
+                        <img src="image source" class="img-fluid rounded-top" alt="img" style={{ width: "100%", height: "100px" }} />
+                        <p>Name</p>
+                        <p>Price</p>
+                    </div>
+                    <div className="card" style={{ width: "20%", padding: "10px" }}>
+                        <img src="image source" class="img-fluid rounded-top" alt="img" style={{ width: "100%", height: "100px" }} />
+                        <p>Name</p>
+                        <p>Price</p>
+                    </div>
+                    <div className="card" style={{ width: "20%", padding: "10px" }}>
+                        <img src="image source" class="img-fluid rounded-top" alt="img" style={{ width: "100%", height: "100px" }} />
+                        <p>Name</p>
+                        <p>Price</p>
+                    </div>
+                    <div className="card" style={{ width: "20%", padding: "10px" }}>
+                        <img src="image source" class="img-fluid rounded-top" alt="img" style={{ width: "100%", height: "100px" }} />
+                        <p>Name</p>
+                        <p>Price</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
