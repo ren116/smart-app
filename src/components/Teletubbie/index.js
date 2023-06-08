88de4968088cad2816be4dab8c33d127e83ec892
FireMedusa <yusukenishikado96@gmail.com>
2023-06-09 04:58:14 +0900

    Added search component and feature

--- src/components/Teletubbie/index.js
+++ src/components/Teletubbie/index.js
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import { Box } from '@mui/material'
-import Paper from '@mui/material/Paper';
-import InputBase from '@mui/material/InputBase';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
 
 const Telebubbie = (props) => {
     return (
@@ -41,21 +37,3 @@ const Telebubbie = (props) => {
     )
 }
 export default Telebubbie;
-
-export const SearchField = (props) => {
-    return (
-        <Paper
-            component="form"
-            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
-        >
-            <InputBase
-                sx={{ ml: 1, flex: 1 }}
-                placeholder ={props.placeholder}
-               onChange ={props.onChange}
-            />
-            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-                <SearchIcon />
-            </IconButton>
-        </Paper>
-    )
-}
