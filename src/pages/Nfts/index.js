45c0662bb2f7a27ce8c5b0ffb13fb4d9d6d3dc8b
Schmidt <atlassmith95@gmail.com>
2023-06-08 16:58:28 +0900

    add: blogbox

--- src/pages/Nfts/index.js
+++ src/pages/Nfts/index.js
@@ -3,13 +3,11 @@ import Search from "components/Search";
 import { Container, Typography, Box } from "@mui/material";
 
 const Nfts = () => {
-
     return (
         <Box sx={{ bgcolor: '#bcaaa4',}}>
         <Box sx={{ display: "flex", justifyContent: "center", paddingTop: '60px' }}>
             <Blog />
         </Box>
-
     </Box>
     )
 };
