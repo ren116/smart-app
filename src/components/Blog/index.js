d3d2f4a76f9e19db8d3c60d5d27309b8b62adcd0
Schmidt <atlassmith95@gmail.com>
2023-06-08 17:06:01 +0900

    add: blogbox

--- src/components/Blog/index.js
+++ src/components/Blog/index.js
@@ -25,6 +25,20 @@ const Blog = () => {
                     <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
                 </Paper>
             </Box>
+            <Container maxWidth="xl">
+                <ImageList sx={{ width: 500, height: 450 }} cols={3} rowHeight={164}>
+                    {itemData.map((item) => (
+                        <ImageListItem key={item.img}>
+                            <img
+                                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                alt={item.title}
+                                loading="lazy"
+                            />
+                        </ImageListItem>
+                    ))}
+                </ImageList>
+            </Container>
         </div>
     )
 }
