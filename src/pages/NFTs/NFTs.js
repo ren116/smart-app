563a7ce1e348180fd9d65ae3f1a4975b29e8cf34
pabaross <pababravo@gmail.com>
2023-06-08 17:56:21 +0900

    import Material UI elements and create NFT Header

--- src/pages/NFTs/NFTs.js
+++ src/pages/NFTs/NFTs.js
@@ -1,7 +1,11 @@
-export default function NFTs(){
-    return(
-        <div>
-            This is NFTs Page.
-        </div>
+import React, { useState, useEffect } from 'react'
+import { CardMedia, CardContent, Typography, CardActionArea, Card } from '@mui/material';
+import { Container, Grid, TextField } from '@mui/material';
+
+export default function NFTs() {
+    return (
+        <Typography gutterBottom variant="h3" component="div" sx={{ paddingTop: 5 }}>
+            NFT Marketplace
+        </Typography>
     );
 }
