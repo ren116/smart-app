27bfe169e3a7ccbdfe6da5c7a7568ab532fa38c0
collinwhite <whitecollin1113@gmail.com>
2023-06-08 14:17:09 +0900

    add: get data feature to teletubbies page

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { Container, Typography, Box } from '@mui/material'
 import Teletubbie from 'components/Teletubbie'
+import { getData } from 'api'
 
 const Teletubbies = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    (async function () {
+      const data = await getData()
+      setData(data)
+    })()
+  }, []);
   return (
     <Container maxWidth='xl'>
       <Typography variant='h1' sx={{ marginTop: '30px' }}>
