08d1a3cbd8f129c4c9a7312ecd95c6fcaf7ade02
Coding Star <jsstar580@gmail.com>
2023-06-08 12:23:30 -0700

    add: mui/material

--- src/pages/Buildings/index.js
+++ src/pages/Buildings/index.js
@@ -1,4 +1,14 @@
 import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { Margin } from "@mui/icons-material";
 
 const Buildings = () => {
   return (
