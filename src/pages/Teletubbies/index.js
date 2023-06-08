e991409cb1756012e41fd2bb25f780e05180948a
carisno <carisnoremes@gmail.com>
2023-06-09 02:32:17 +0900

    fixed delete no need tag

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -18,89 +18,87 @@ const Teletubbies = () => {
   }, []);
 
   return (
-    <>
-      <Container>
-        <Grid>
-          <Typography
-            variant="h2"
-            sx={{
-              border: "3px solid",
-              borderColor: "primary.main",
-              color: "Blue",
-              padding: 4,
-              boxShadow: 3,
-            }}
-            mt={4}
-            mb={3}
-          >
-            Teletubbies
-          </Typography>
-        </Grid>
-        {teletubbies.map((teletubby, index) => (
-          <Grid key={index}>
-            <>
-              <Grid
-                container
-                alignItems="center"
-                sx={{
-                  border: "2px solid",
-                  borderColor: "success.main",
-                  boxShadow: 2,
-                }}
-                p={2}
-                my={2}
-                direction={`${index % 2 ? "row-reverse" : ""}`}
-              >
-                <Grid item xs={12} md={3} lg={3}>
-                  <div className="img-container">
-                    <img
-                      src={teletubby.image_url}
-                      alt={teletubby.image_url}
-                      style={{
-                        width: "180px",
-                        height: "180px",
-                        padding: "auto",
-                      }}
-                    />
-                  </div>
-                </Grid>
+    <Container>
+      <Grid>
+        <Typography
+          variant="h2"
+          sx={{
+            border: "3px solid",
+            borderColor: "primary.main",
+            color: "Blue",
+            padding: 4,
+            boxShadow: 3,
+          }}
+          mt={4}
+          mb={3}
+        >
+          Teletubbies
+        </Typography>
+      </Grid>
+      {teletubbies.map((teletubby, index) => (
+        <Grid key={index}>
+          <>
+            <Grid
+              container
+              alignItems="center"
+              sx={{
+                border: "2px solid",
+                borderColor: "success.main",
+                boxShadow: 2,
+              }}
+              p={2}
+              my={2}
+              direction={`${index % 2 ? "row-reverse" : ""}`}
+            >
+              <Grid item xs={12} md={3} lg={3}>
+                <div className="img-container">
+                  <img
+                    src={teletubby.image_url}
+                    alt={teletubby.image_url}
+                    style={{
+                      width: "180px",
+                      height: "180px",
+                      padding: "auto",
+                    }}
+                  />
+                </div>
+              </Grid>
 
-                <Grid item xs={12} md={9} lg={9}>
-                  <Grid component="div">
-                    <Typography
-                      variant="h4"
-                      component="div"
-                      align="left"
-                      sx={{ px: 2, my: 1 }}
-                    >
-                      {teletubby.name}
-                    </Typography>
-                    <Typography
-                      variant="body1"
-                      component="div"
-                      sx={{ px: 2, my: 1 }}
-                    >
-                      {teletubby.description}
-                    </Typography>
-                    <Grid container spacing={2} justifyContent="flex-start">
-                      {teletubby.traits.map((item, key) => {
-                        return (
-                          <Grid item key={key} my={1}>
-                            <Paper variant="outlined" sx={{ p: 1 }}>
-                              {item}
-                            </Paper>
-                          </Grid>
-                        );
-                      })}
-                    </Grid>
+              <Grid item xs={12} md={9} lg={9}>
+                <Grid component="div">
+                  <Typography
+                    variant="h4"
+                    component="div"
+                    align="left"
+                    sx={{ px: 2, my: 1 }}
+                  >
+                    {teletubby.name}
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    component="div"
+                    sx={{ px: 2, my: 1 }}
+                  >
+                    {teletubby.description}
+                  </Typography>
+                  <Grid container spacing={2} justifyContent="flex-start">
+                    {teletubby.traits.map((item, key) => {
+                      return (
+                        <Grid item key={key} my={1}>
+                          <Paper variant="outlined" sx={{ p: 1 }}>
+                            {item}
+                          </Paper>
+                        </Grid>
+                      );
+                    })}
                   </Grid>
                 </Grid>
               </Grid>
-            </>
-          </Grid>
-        ))}
-      </Container>
-    </>
+            </Grid>
+          </>
+        </Grid>
+      ))}
+    </Container>
   );
 };
 
