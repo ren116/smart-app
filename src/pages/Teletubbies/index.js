f4850c20089b867eaf86439c9537102afe1dddda
carisno <carisnoremes@gmail.com>
2023-06-08 18:39:48 +0900

    import teletubbies.json & add map function

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -1,36 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Container, Typography, Box, Grid } from "@mui/material";
-
-import {} from "api";
+import teletubbiesData from "../../Data/teletubbies.json";
 
 const Tele = () => {
- 
-
- 
-
-  useEffect(() => {
-
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <>
       <Container>
         <Typography variant="h4">Teletubbies</Typography>
-        <Grid container spacing={2}>
-          <Grid item>
-            <img src="" alt="1" />
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item>
-                <Typography variant="h5">Tinky Winky</Typography>
-              </Grid>
-              <Grid item>
-                <Box sx={{ display: "flex", alignItems: "center" }}></Box>
+        {teletubbiesData.map((teletubby, index) => (
+          <Grid container spacing={2} key={index}>
+            <Grid item>
+              <img src={teletubby.image_url} />
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item xs container direction="column" spacing={2}>
+                <Grid item>
+                  <Typography variant="h5">{teletubby.name}</Typography>
+                </Grid>
+                <Grid item>
+                  <Box sx={{ display: "flex", alignItems: "center" }}></Box>
+                </Grid>
               </Grid>
             </Grid>
           </Grid>
-        </Grid>
+        ))}
       </Container>
     </>
   );
