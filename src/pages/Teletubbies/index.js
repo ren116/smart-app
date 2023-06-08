5901de08bba0cf46b73ee743e903f9d0e702be98
collinwhite <whitecollin1113@gmail.com>
2023-06-08 14:10:36 +0900

    add: search feature to teletubbies page

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Typography, Box } from '@mui/material'
+import { Container, Typography } from '@mui/material'
+import TextField from '@mui/material/TextField'
 import Teletubbie from 'components/Teletubbie'
 
 const Teletubbies = () => {
   const [data, setData] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const handleSearch = event => {
+    setSearchTerm(event.target.value.toLowerCase())
+  }
   return (
     <Container maxWidth='xl'>
       <Typography variant='h1' sx={{ marginTop: '30px' }}>
         Teletubbies
       </Typography>
-      <TextField id='outlined-basic' label='Search by name' variant='outlined' />
+      <TextField
+        id='outlined-basic'
+        value={searchTerm}
+        onChange={handleSearch}
+        label='Search by name'
+        variant='outlined' />
       {data
          .map((item, key) => {
            return (
