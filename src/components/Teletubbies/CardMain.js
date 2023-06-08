a6f789cf4f018db4b349dd45600e548576046dfb
Lucky Miner <andreasfischer0201@gmail.com>
2023-06-08 14:12:52 -0700

    ops: add bubble font

--- src/components/Teletubbies/CardMain.js
+++ src/components/Teletubbies/CardMain.js
@@ -2,8 +2,8 @@ import { CardContent, Typography, Chip } from '@mui/material';
 
 export const CardMain = (props) => {
   return (
-    <CardContent align="left" sx={{height: "100px", padding: "10px", paddingBottom: "0px !important",}}>
-      <Typography gutterBottom variant="h5" component="div">
+    <CardContent align="left" sx={{height: "100px", padding: "10px", paddingBottom: "0px !important", fontFamily: "bubble !important"}}>
+      <Typography gutterBottom variant="h5" component="div" sx={{color: props.teletubby.color}}>
         {props.teletubby.name}
       </Typography>
       <Typography variant="body2" color="text.secondary">
