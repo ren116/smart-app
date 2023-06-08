4c0450cfbcee9372c3430df5f12df2e1b32d90d0
carisno <carisnoremes@gmail.com>
2023-06-08 18:52:23 +0900

    update ui in index.js

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Typography, Box, Grid } from "@mui/material";
+import { Container, Typography, Grid, Paper } from "@mui/material";
 import teletubbiesData from "../../Data/teletubbies.json";
 
 const Teletubbies = () => {
@@ -10,20 +10,71 @@ const Teletubbies = () => {
       <Container>
         <Typography variant="h4">Teletubbies</Typography>
         {teletubbiesData.map((teletubby, index) => (
-          <Grid container spacing={2} key={index}>
-            <Grid item>
-              <img src={teletubby.image_url} />
-            </Grid>
-            <Grid item xs={12} sm container>
-              <Grid item xs container direction="column" spacing={2}>
-                <Grid item>
-                  <Typography variant="h5">{teletubby.name}</Typography>
+          <Grid key={index}>
+            <>
+              <Grid
+                container
+                sx={{
+                  border: "2px solid",
+                  borderColor: "success.main",
+                  position: "relative",
+                }}
+                p={2}
+                my={2}
+              >
+                <Grid item xs={12} md={3} lg={3}>
+                  <div className="img-container">
+                    <img
+                      src={teletubby.image_url}
+                      alt={teletubby.image_url}
+                      style={{
+                        width: "180px",
+                        height: "180px",
+                        padding: "auto",
+                      }}
+                    />
+                  </div>
                 </Grid>
-                <Grid item>
-                  <Box sx={{ display: "flex", alignItems: "center" }}></Box>
+
+                <Grid item xs={12} md={9} lg={9}>
+                  <Grid component="div">
+                    <Typography
+                      variant="h4"
+                      component="div"
+                      align="left"
+                      sx={{ px: 2 }}
+                    >
+                      {teletubby.name}
+                    </Typography>
+                    <Typography variant="body1" component="div" sx={{ px: 2 }}>
+                      {teletubby.description}
+                    </Typography>
+                    <Grid container spacing={2} justifyContent="flex-start">
+                      <Grid item>
+                        <Paper variant="outlined" sx={{ p: 1 }}>
+                          {teletubby.traits[0]}
+                        </Paper>
+                      </Grid>
+                      <Grid item>
+                        <Paper variant="outlined" sx={{ p: 1 }}>
+                          {teletubby.traits[1]}
+                        </Paper>
+                      </Grid>
+                      <Grid item>
+                        <Paper variant="outlined" sx={{ p: 1 }}>
+                          {teletubby.traits[2]}
+                        </Paper>
+                      </Grid>
+                      <Grid item>
+                        <Paper variant="outlined" sx={{ p: 1 }}>
+                          {teletubby.traits[3]}
+                        </Paper>
+                      </Grid>
+                    </Grid>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
+            </>
           </Grid>
         ))}
       </Container>
