a1e5c3e37128097095ef9e2d7ecfe85ebfff0d9a
collinwhite <whitecollin1113@gmail.com>
2023-06-08 14:20:09 +0900

    add scroll feature to teletubbies page

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -5,18 +5,34 @@ import { getData } from 'api'
 
 const Teletubbies = () => {
   const [data, setData] = useState([]);
+  const [offset,setOffset]=useState(0);
+
   useEffect(() => {
     (async function () {
       const data = await getData()
       setData(data)
     })()
   }, []);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        window.innerHeight + window.scrollY >=
+        document.body.offsetHeight - 1000
+      ) {
+        setOffset(offset+20);
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [offset])
   return (
     <Container maxWidth='xl'>
       <Typography variant='h1' sx={{ marginTop: '30px' }}>
         Teletubbies
       </Typography>
-      {data
+      {data.slice(0,20+offset)
          .map((item, key) => {
            return (
              <Teletubbie
