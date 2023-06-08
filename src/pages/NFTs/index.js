0901a1ab90bc0a46c7b2e2690ee375dc229a27e8
pabaross <pababravo@gmail.com>
2023-06-09 04:08:17 +0900

    add filteredNftListing varies

--- src/pages/NFTs/index.js
+++ src/pages/NFTs/index.js
@@ -1,6 +1,7 @@
 import { useState,useEffect } from "react";
 export default function NFTs() {
   const [nftListing, setNftListing] = useState([]);
+  const [filteredNftListing, setFilteredNftListing] = useState([])
   const loadNftListing = async () => {
     const response = await fetch(
       `https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=20&offset=0`
