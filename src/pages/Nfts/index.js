0d4f027aa7dff87e8b7addd24b20c5881e1eb8bd
lysan0333 <135338706+lysan0333@users.noreply.github.com>
2023-06-09 01:27:35 -0700

    update: NFTs filter as image collectionname and price in index.js in NFTs folder

--- src/pages/Nfts/index.js
+++ src/pages/Nfts/index.js
@@ -1,9 +1,26 @@
-import React from 'react'
+giimport React, { useState, useEffect } from 'react'
 import { CardMedia, CardContent, Typography, CardActionArea } from '@mui/material';
 import { Container, Grid, TextField } from '@mui/material';
 import Header from 'components/Header';
 
 const Nfts = () => {
+    const [nftListing, setNftListing] = useState([])
+    const [filteredNftListing, setFilteredNftListing] = useState([])
+    const [offset, setOffset] = useState(0)
+    const loadNftListing = async () => {
+        const response = await fetch(
+            `https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=20&offset=${offset}`
+        )
+        const data = await response.json()
+        setNftListing([...nftListing, ...data.results])
+        setFilteredNftListing([...nftListing, ...data.results])
+        setOffset(offset + 20)
+        console.log(response)
+    }
+    useEffect(() => {
+        loadNftListing()
+    }, [])
+    
     return (
         <div>
             <Header/>
@@ -18,23 +35,26 @@ const Nfts = () => {
             <Container maxWidth="lg">
                 <div id='nftListing'>
                     <Grid container spacing={3}>
-                            <Grid className="grid" item  lg={3} md={4} sm={6} xs={12}>
+                        {filteredNftListing.map(nftListing => (
+                            <Grid key={nftListing.id} className="grid" item  lg={3} md={4} sm={6} xs={12}>
                                 <CardActionArea sx={{backgroundColor:'black'}}>
                                     <CardMedia
                                         component="img"
                                         height="auto"
-                                       
+                                        image={nftListing.img}
+                                        alt={nftListing.name}
                                     />
                                     <CardContent sx={{display:'flex',justifyContent:"space-between",color:"white"}}>
                                         <Typography gutterBottom variant="p" component="h3">
-                                            {}
+                                            {nftListing.collectionName}
                                         </Typography>
                                         <Typography variant="p" component="h3">
-                                            {}$
+                                            {nftListing.price}$
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
                             </Grid>
+                        ))}
                     </Grid>
                 </div>
             </Container>
