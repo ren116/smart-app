87e7adb9c992eb91e3c0409d118ef54fcb289873
Schmidt <atlassmith95@gmail.com>
2023-06-09 09:54:55 +0900

    update: indentation of space

--- src/pages/Nfts/index.js
+++ src/pages/Nfts/index.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from "react";
-import Search from "components/Search";
+import Blog from "components/Blog";
 import { Container, Typography, Box } from "@mui/material";
 
 const Nfts = () => {
     return (
-        <Box sx={{ bgcolor: '#bcaaa4',}}>
-        <Box sx={{ display: "flex", justifyContent: "center", paddingTop: '60px' }}>
-            <Blog />
+        <Box sx={{ bgcolor: '#bcaaa4', }}>
+            <Box sx={{ display: "flex", justifyContent: "center", paddingTop: '60px' }}>
+                <Blog />
+            </Box>
         </Box>
-    </Box>
     )
 };
 
