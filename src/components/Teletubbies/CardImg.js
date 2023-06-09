28033cba809506389626741c76240bd14de55bba
Lucky Miner <andreasfischer0201@gmail.com>
2023-06-09 14:21:44 -0700

    fix: update indentation in CardImg component

--- src/components/Teletubbies/CardImg.js
+++ src/components/Teletubbies/CardImg.js
@@ -3,11 +3,11 @@ import { CardMedia } from '@mui/material';
 export const CardImg = (teletubby) => {
   return (
     <CardMedia
-        component="img"
-        alt={teletubby.name}
-        image={teletubby.image_url}
-        title={teletubby.name}
-        sx={{minWidth: "100px", width: "100px", height: "100px"}}
-      />
+      component="img"
+      alt={teletubby.name}
+      image={teletubby.image_url}
+      title={teletubby.name}
+      sx={{minWidth: "100px", width: "100px", height: "100px"}}
+    />
   )
 } 
