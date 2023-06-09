d0ce6be859c32a3d0119d8d0e8838cae00a444a4
collinwhite <whitecollin1113@gmail.com>
2023-06-09 10:00:21 +0900

    update: spacing and error handling

--- src/pages/Teletubbies/index.js
+++ src/pages/Teletubbies/index.js
@@ -2,15 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { Container, Typography } from '@mui/material'
 import TextField from '@mui/material/TextField'
 import Teletubbie from 'components/Teletubbie'
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 import { getTeletubbies } from 'api'
 
 const Teletubbies = () => {
   const [teletubbies, setTeletubbies] = useState([])
   const [offset, setOffset] = useState(0)
   const [searchWord, setSearchWord] = useState('')
+  const [alert, setAlert] = React.useState(false);
+  
   const handleSearch = event => {
     setSearchWord(event.target.value.toLowerCase())
   }
+
   useEffect(() => {
     try{
       (async function () {
@@ -19,14 +25,14 @@ const Teletubbies = () => {
       })()
     }
     catch(err){
-      throw err;
+      setAlert(true);
     }
   }, []);
+  
   useEffect(() => {
     const handleScroll = () => {
       if (
-        window.innerHeight + window.scrollY >=
-        document.body.offsetHeight 
+        window.innerHeight + window.scrollY >= document.body.offsetHeight 
       ) {
         setOffset(offset+20);
       }
@@ -36,6 +42,25 @@ const Teletubbies = () => {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [offset])
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setAlert(false);
+  };
+  const show = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
   return (
     <Container maxWidth='xl'>
       <Typography variant='h1' sx={{ marginTop: '30px' }}>
@@ -47,6 +72,13 @@ const Teletubbies = () => {
         onChange={handleSearch}
         label='Search by name'
         variant='outlined' />
+        <Snackbar
+        open={alert}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Fail in load"
+        action={show}
+      />
       {teletubbies.slice(0 , 20+offset)
         .filter(list=>list.name.toLowerCase().includes(searchWord))
         .map((item, key) => {
