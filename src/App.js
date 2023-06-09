69fb6e7d90316d1f1401c7f61fb00d79ee2b9bb6
lysan0333 <135338706+lysan0333@users.noreply.github.com>
2023-06-09 00:47:16 -0700

    feat: NFTs router in app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "pages/Layout";
 import Home from "pages/Home";
 import NoPage from "pages/NoPage";
+import Nfts from "pages/Nfts/index";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="*" element={<NoPage />} />
           </Route>
+          <Route path="/Nfts" element={<Nfts/>} />
         </Routes>
       </BrowserRouter>
     </div>
