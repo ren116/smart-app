4639b416b7d741feaf0e776f2e6a8ea323372536
Lucky Miner <andreasfischer0201@gmail.com>
2023-06-09 16:17:00 -0700

    fix: remove unnecessary file and update props variable name in CardImg component

--- src/components/Teletubbies/CardImg.js
+++ src/components/Teletubbies/CardImg.js
@@ -4,9 +4,9 @@ export const CardImg = (props) => {
   return (
     <CardMedia
       component="img"
-      alt={props.teletubby.name}
-      image={props.teletubby.image_url}
-      title={props.teletubby.name}
+      alt={props.cardInfo.name}
+      image={props.cardInfo.image_url}
+      title={props.cardInfo.name}
       sx={{ minWidth: "100px", width: {sm: "100px", xs: "50%"}, height: 
         {sm: "100px", xs: "auto"}, m: {xs: "auto", sm: 0} }}
     />
