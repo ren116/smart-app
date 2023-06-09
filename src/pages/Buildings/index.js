c443c929b88d1c2feab0f268a9edb496b498f1ff
hansengentle <Hansen.gentle@gmail.com>
2023-06-09 01:59:39 -0700

    fix: Fixed some problems that indicated

--- src/pages/Buildings/index.js
+++ src/pages/Buildings/index.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { Container } from "@mui/material";
 import Building from "../../components/Buildings/Building";
-import buildingsData from "./buildings.json";
+import { Container } from "@mui/material";
+import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 import { Grid, TextField, Typography, Box } from "@mui/material";
+import buildingsData from "./buildings.json";
 
-let PAGE_SIZE = 20;
+var PAGE_SIZE = 20;
 
 const BuildingList = () => {
   const [buildings, setBuildings] = useState(buildingsData.buildings);
@@ -14,6 +15,10 @@ const BuildingList = () => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
+  useEffect(() => {
+    PAGE_SIZE += 3;
+  }, [buildings]);
 
   const handleScroll = () => {
     if (
@@ -26,10 +31,6 @@ const BuildingList = () => {
     setBuildings((prevBuildings) => prevBuildings.concat([]));
   };
 
-  useEffect(() => {
-    PAGE_SIZE += 3;
-  }, [buildings]);
-
   const filterBuildingsByName = (building) => {
     return building.Name.toLowerCase().includes(searchQuery.toLowerCase());
   };
@@ -40,10 +41,13 @@ const BuildingList = () => {
   );
 
   return (
-    <Grid container className="flex justify-center items-center">
-      <Container maxWidth="xl" className="mt-10">
-        <Grid container item xs={12} sm={12} md={12}>
-          <Grid item sm></Grid>
+    <Grid
+      container
+      sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+    >
+      <Container maxWidth="lg" sx={{ mt: 3 }}>
+        <Grid container item xs={12} sm={12} md={12} sx={{ mt: 5 }}>
+          <Grid item sm />
           <Grid item md={8} xs={12}>
             <TextField
               label="Search Buildings"
@@ -53,42 +57,97 @@ const BuildingList = () => {
               onChange={(event) => setSearchQuery(event.target.value)}
             />
           </Grid>
-          <Grid item sm></Grid>
+          <Grid item sm />
         </Grid>
-        <div className="mt-5">
-          <Grid container item xs={12} sm={12} md={12}>
-            <Grid item sm xs></Grid>
-            <Grid
-              container
-              columnSpacing={2}
-              item
-              md={8}
+        <Grid container item xs={12} sm={12} md={12} my={3} sx={{ mt: 5 }}>
+          <Grid item sm xs />
+          <Grid
+            container
+            item
+            justifyContent="space-around"
+            spacing={1}
+            sx={{ display: "flex", flexDirection: "row" }}
+          >
+            <Box
               xs={12}
-              justifyContent="space-around"
+              sm={2}
+              md={2}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
             >
-              <Box item sm={2} xs={2}>
-                <Typography>Site</Typography>
-              </Box>
-              <Box item sm={4} xs={2}>
-                <Typography>Alerts</Typography>
-              </Box>
-              <Box item sm={2} xs={4}>
-                <Typography>Savings</Typography>
-              </Box>
-              <Box item sm={2} xs={2}>
-                <Typography>Uptime</Typography>
-              </Box>
-              <Box item sm={2} xs={2}>
-                <Typography>Power</Typography>
-              </Box>
-            </Grid>
-            <Grid item sm xs></Grid>
+              <Typography sx={{ fontSize: { xs: "14px", md: "16px" } }}>
+                Site
+              </Typography>
+              <KeyboardArrowDown
+                sx={{ fontSize: { xs: "14px", md: "16px" } }}
+              />
+            </Box>
+            <Box
+              xs={12}
+              sm={4}
+              md={4}
+              sx={{ display: "flex", flexDirection: "row" }}
+            >
+              <Typography sx={{ fontSize: { xs: "14px", md: "16px" } }}>
+                Alerts
+              </Typography>
+              <KeyboardArrowDown
+                sx={{ fontSize: { xs: "14px", md: "16px" } }}
+              />
+            </Box>
+            <Box
+              xs={12}
+              sm={2}
+              md={2}
+              sx={{ display: "flex", flexDirection: "row" }}
+            >
+              <Typography sx={{ fontSize: { xs: "14px", md: "16px" } }}>
+                Savings
+              </Typography>
+              <KeyboardArrowDown
+                sx={{ fontSize: { xs: "14px", md: "16px" } }}
+              />
+            </Box>
+            <Box
+              xs={12}
+              sm={2}
+              md={2}
+              sx={{ display: "flex", flexDirection: "row" }}
+            >
+              <Typography sx={{ fontSize: { xs: "14px", md: "16px" } }}>
+                Uptime
+              </Typography>
+              <KeyboardArrowDown
+                sx={{ fontSize: { xs: "14px", md: "16px" } }}
+              />
+            </Box>
+            <Box
+              xs={12}
+              sm={2}
+              md={2}
+              sx={{ display: "flex", flexDirection: "row" }}
+            >
+              <Typography sx={{ fontSize: { xs: "14px", md: "16px" } }}>
+                Power
+              </Typography>
+              <KeyboardArrowDown
+                sx={{ fontSize: { xs: "14px", md: "16px" } }}
+              />
+            </Box>
           </Grid>
-        </div>
+          <Grid item sm xs />
+        </Grid>
       </Container>
       {filteredBuildings
         .slice(0, PAGE_SIZE)
-        .map((building) => building && <Building building={building} />)}
+        .map(
+          (building, index) =>
+            !!building && <Building building={building} key={index} />
+        )}
     </Grid>
   );
 };
