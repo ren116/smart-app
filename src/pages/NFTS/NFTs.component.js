a7d54735034ae9ebef5c999ce1f48dfb7e02e371
Zinedine <zinedinedumas@gmail.com>
2023-06-09 13:30:12 +0900

    Changed the style and variable name

--- src/pages/NFTS/NFTs.component.js
+++ src/pages/NFTS/NFTs.component.js
@@ -6,36 +6,36 @@ import { Grid } from "@mui/material";
 import Box from "@mui/material/Box";
 
 const Nfts_PAGE = () => {
-  const [listing, setListing] = useState([]);
+  const [list, setListing] = useState([]);
   const [offset, setOffset] = useState(0);
   const [searchTerm, setSearchTerm] = useState("");
-  const [filteredListing, setFilteredListing] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
 
-  const loadListing = useCallback(async () => {
+  const loadList = useCallback(async () => {
     const response = await fetch(
       `https://api-mainnet.magiceden.io/idxv2/getListedNftsByCollectionSymbol?collectionSymbol=okay_bears&limit=20&offset=${offset}`
     );
     const data = await response.json();
-    setListing([...listing, ...data.results]);
-    setFilteredListing([...listing, ...data.results]);
+    setListing([...list, ...data.results]);
+    setFilteredList([...list, ...data.results]);
     setOffset(offset + 20);
-  }, [offset, listing]);
+  }, [offset, list]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value.toLowerCase());
   };
 
   useEffect(() => {
-    loadListing();
-  }, [loadListing]);
+    loadList();
+  }, [loadList]);
 
   useEffect(() => {
-    setFilteredListing(
-      listing.filter((listing) =>
-        listing.collectionName.toLowerCase().includes(searchTerm)
+    setFilteredList(
+      list.filter((list) =>
+        list.collectionName.toLowerCase().includes(searchTerm)
       )
     );
-  }, [listing, searchTerm]);
+  }, [list, searchTerm]);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -43,18 +43,18 @@ const Nfts_PAGE = () => {
         window.innerHeight + window.scrollY >=
         document.body.offsetHeight - 1000
       ) {
-        loadListing();
+        loadList();
       }
     };
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [loadListing]);
+  }, [loadList]);
 
   return (
-    <Container maxWidth="xl">
-      <Typography variant="h2" sx={{ m: "60px" }}>
+    <Container maxWidth="xl" sx={{ justifyContent: "center", margin: "auto" }}>
+      <Typography variant="h2" sx={{ m: "60px", justifyContent: "center" }}>
         NFT Marketplace
       </Typography>
       <TextField
@@ -71,12 +71,13 @@ const Nfts_PAGE = () => {
         container
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
+        sx={{ m: "auto" }}
       >
-        {filteredListing.map((listing) => (
-          <Grid item className="card" key={listing.id} xs={3}>
+        {filteredList.map((list) => (
+          <Grid item className="card" key={list.id} xs={3}>
             <img
               style={{ width: "100%", height: "70%" }}
-              src={listing.img}
+              src={list.img}
               alt={""}
               variant="img"
             />
@@ -84,8 +85,8 @@ const Nfts_PAGE = () => {
               sx={{ display: "flex", justifyContent: "space-between" }}
               style={{ backgroundColor: "orange" }}
             >
-              <Typography variant="h5">{listing.collectionName}</Typography>
-              <Typography variant="p">{listing.price}</Typography>
+              <Typography variant="h5">{list.collectionName}</Typography>
+              <Typography variant="p">{list.price}</Typography>
             </Box>
           </Grid>
         ))}
