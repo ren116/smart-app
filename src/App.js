5ed964ab3b074ea7d6d06c2e061a3e6af90eedaf
Lucky Miner <andreasfischer0201@gmail.com>
2023-06-09 18:05:48 -0700

    resolved: conflicts in App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
             <Route path="*" element={<NoPage />} />
-            <Route path="teletubbies" element={<Teletubbies />} />
           </Route>
         </Routes>
       </BrowserRouter>
