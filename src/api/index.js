9b250a72cdf290efcb3e8c41c003ad0033825bb6
joel <shine401020@gmail.com>
2023-07-09 09:44:43 +0200

    fixed: location to my home

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const weatherOptions = {
   method: "GET",
   url: "https://weatherapi-com.p.rapidapi.com/current.json",
-  params: { q: "22.3, 114.1" },
+  params: { q: "51.24, 6.77" },
   headers: {
     "X-RapidAPI-Key": "cf1d6bd258msh581225dc1a5493fp11399bjsn09a7b76afe60",
     "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
